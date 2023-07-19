db69d5a2cd1898d0e8300e7b86c2bf44cb731931
project calculator <ritamagdy99@gmail.com>
2023-07-19 18:43:15 +0300

    cloumn and text are better in center

--- lib/news/News_Item.dart
+++ lib/news/News_Item.dart
@@ -19,32 +19,40 @@ class NewsItem extends StatelessWidget {
             margin: EdgeInsets.all(12),
             padding: EdgeInsets.all(10),
             height: 220,
-
             // 7tete al round al fl sora
             clipBehavior: Clip.antiAlias,
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-
-
-
             child: Image.network(news.urlToImage ?? ''),
           ),
-          Text(
-            news.author ?? '',
-            style: TextStyle(fontSize: 20),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  news.author ?? '',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  news.title ?? '',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  news.publishedAt ?? '',
+                  style: TextStyle(fontSize: 12),
+                  textAlign: TextAlign.end,
+                ),
+
+              ],
+            ),
           ),
-          Text(
-            news.title ?? '',
-            style: TextStyle(fontSize: 20),
-          ),
-          Text(
-            news.publishedAt ?? '',
-            style: TextStyle(fontSize: 12),
-            textAlign: TextAlign.end,
-          ),
+
         ],
       ),
     );
   }
+
+
+
 }
 
 
--- lib/news/news_details_Screen.dart
+++ lib/news/news_details_Screen.dart
@@ -8,57 +8,66 @@ class NewsDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var news = ModalRoute.of(context)!.settings.arguments as News;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("News details"),
+        centerTitle: true,
+      ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            margin: EdgeInsets.all(12),
+            margin: EdgeInsets.all(15),
+            padding: EdgeInsets.all(5),
+            height: 220,
             // 7tete al round al fl sora
             clipBehavior: Clip.antiAlias,
             decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
 
             child: Image.network(news.urlToImage ?? ''),
           ),
-          Text(
-            news.author ?? '',
-            style: TextStyle(
-              fontSize: 12,
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  news.author ?? '',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.end,
+                ),
+                Text(
+                  news.title ?? '',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  news.publishedAt ?? '',
+                  style: TextStyle(fontSize: 12),
+                  textAlign: TextAlign.end,
+                ),
+                SizedBox(height: 40),
+                Text(news.description ?? ''),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton.icon(
+                      onPressed: () => LaunchUrl(news.url),
+                      label: Text("View more about the article"),
+                      icon: Icon(Icons.arrow_forward),
+                    ),
+                  ],
+                )
+              ],
             ),
-            textAlign: TextAlign.end,
           ),
-          Text(
-            news.title ?? '',
-            style: TextStyle(fontSize: 18),
-          ),
-          Text(
-            news.publishedAt ?? '',
-            style: TextStyle(fontSize: 12),
-            textAlign: TextAlign.end,
-          ),
-          SizedBox(height: 40),
-          Text(news.description ?? ''),
-
-          SizedBox(height: 10),
-
-        Row(
-          children: [
-            TextButton.icon(
-              onPressed: (){},
-                label: Text("View more about the article"),
-              icon: Icon(Icons.arrow_back_ios_sharp),
-
-
-            ),
-
-
-
-          ],
-
-        )
-
         ],
       ),
     );
+  }
+
+  LaunchUrl(String? Url) {
+    if (Url == null) {
+      return;
+    }
   }
 }
